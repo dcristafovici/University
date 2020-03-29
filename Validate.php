3f3c332ca5e0bf5cebd9ef5b7e687f3bd809b2f9
denibudeyko <denbudeyko@gmail.com>
2020-03-29 06:04:38 +0300

    validate

---
+++ Validate.php
@@ -0,0 +1,91 @@
+<?php
+
+
+class Validate
+{
+  private $db = null, $errors = [], $success = false;
+
+  public function __construct()
+  {
+    $this->db = Database::getInstance();
+  }
+
+
+  public function check($source, $fields ){
+
+		foreach ($fields as $item=>$rules) {
+			
+			foreach ($rules as $rule =>$rule_value) {
+				
+				$value = $source[$item];
+			
+				if($rule == 'required' && empty($value)){
+					$this->addError("{$item} объязателен для заполнения");
+				}
+				else if(!empty($value)){
+					
+					switch ($rule){
+						
+						case 'min':
+							if(strlen($value) < $rule_value){
+								$this->addError("{$item} должен иметь больше {$rule_value} символов");
+							}
+						break;
+							
+						case 'max':
+							if(strlen($value) > $rule_value){
+								$this->addError("{$item} должен иметь меньше {$rule_value} символов");
+							}
+						break;
+							
+						case 'matches':
+							if($value != $source[$rule_value]){
+								$this->addError("{$rule_value} должен совпадать с {$item}" );
+							}
+						break;
+							
+						case 'unique';
+							$check = $this->db->get($rule_value, [$item, '=', $value]);
+							if($check->count()){
+								$this->addError("${item} уже существует в базе данных");
+							}
+						break;
+						
+					}
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+			}
+
+
+		}
+		if(empty($this->errors)){
+			$this->success = true;
+		}
+		return $this;
+  }
+
+  public function addError($error){
+
+    $this->errors[] = $error;
+
+  }
+
+  public function showErrors(){
+    return $this->errors;
+
+  }
+
+  public function success(){
+
+  	return $this->success;
+
+	}
+
+}
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 //Require Once Files
 require_once "Database.php";
 require_once "Config.php";
-
-
+require_once "Input.php";
+require_once "Validate.php";
 
 $GLOBALS['config']= [
   'mysql' =>[
@@ -21,7 +21,7 @@ $GLOBALS['config']= [
 
 
 
-$products = Database::getInstance()->get('products', ['name', '=', 'Tested']);
+//$products = Database::getInstance()->get('products', ['name', '=', 'Tested']);
 #Database::getInstance()->delete('products', ['name', '=', 'test']);
 //Database::getInstance()->insert('products', [
 //  'name' => 'Tested',
@@ -39,15 +39,80 @@ $products = Database::getInstance()->get('products', ['name', '=', 'Tested']);
 //]);
 //
 
-if($products->showError()){
-  echo 'you have eror';
+//if($products->showError()){
+//  echo 'you have eror';
+//}
+//else{
+//
+//  $results = $products->showResult();
+//  foreach($results as $result){
+//
+//    var_dump($result);
+//
+//  }
+//}
+
+
+if(Input::exists()){
+
+    $validate = new Validate();
+
+    
+    $validation = $validate->check($_POST, [
+			'name' => [
+				'required' => true,
+				'min' => 3,
+				'max' => 6,
+				'unique' => 'products'
+			],
+			'password' => [
+				'required' => true,
+				'min' => 3,
+				'max' => 6,
+			],
+			'password_again' =>[
+			    'required' => true,
+				'matches' => 'password'
+			]
+    
+    ]);
+    
+    if($validate->success()){
+        echo 'passed';
+    }
+    else{
+        $allErrors = $validate->showErrors();
+        foreach($allErrors as $error){
+            echo $error."<br>";
+        }
+     
+    }
+    
+
 }
-else{
 
-  $results = $products->showResult();
-  foreach($results as $result){
+?>
 
-    var_dump($result);
 
-  }
-}
+<form action="" method="post">
+
+  <div class="field">
+    <label for="">Имя пользователя</label>
+    <input type="text" value="<?php echo Input::get('name');?>" name="name">
+  </div>
+
+  <div class="field">
+    <label for="">Пароль</label>
+    <input type="password" name="password">
+  </div>
+
+  <div class="field">
+    <label for="">Повторите пароль</label>
+    <input type="password" name="password_again">
+  </div>
+
+  <div class="field">
+      <button>Отправить</button>
+  </div>
+
+</form>
